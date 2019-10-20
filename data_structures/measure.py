e2d2919cf413de19c73986acf86816441b2e893d
João H <jhmcastelo@gmail.com>
2019-10-20 15:05:04 -0200

    measure_test

--- data_structures/measure.py
+++ data_structures/measure.py
@@ -1,52 +1,59 @@
 import numpy as np
 from datetime import datetime
- 
+from dateutil.parser import parse as dt_parse
+import re
+
 transformations = {
     "p" : 1e-12,
     "n" : 1e-9,
-    "micro" : 1e-6,
+    "u" : 1e-6,
     "m" : 1e-3,
     "c" : 1e-2,
     "d" : 1e-1,
-    "da" : 1e1,
-    "h" : 1e2,
-    "k" : 1e3,
-    "M" : 1e6,
-    "G" : 1e9,
+    "da" : 1e+1,
+    "h" : 1e+2,
+    "k" : 1e+3,
+    "M" : 1e+6,
+    "G" : 1e+9,
 }
 
 class BaseMeasure:
-    def __init__(self,measure:float ,unit : str):
-        self.measure = measure
-        self.unit = unit
+    def __init__(self,value:float ,unit : str):
+        self.value = value
+        self.unit = re.sub('[^A-Za-z0-9]+', '', unit)
     def __str__(self):
-        return f"Value : {self.measure} {self.unit}"
-
+        print_value = "%.1e" % self.value
+        return f"{print_value} {self.unit}"
 
 class Measure(BaseMeasure):
-    def __init__(self,measure:float ,unit : str, transformed : bool = False):
-        super().__init__(self,measure,unit)
-    def decode_measure(self):
+    def __init__(self,value:float ,unit : str, transformed : bool = False):
+        super().__init__(value,unit)
+        self.transformed = transformed
+        self.si_value,self.si_unit = self.decode_value()
+    def decode_value(self):
+        si_value,si_unit = self.value,self.unit
         if self.transformed:
             for transf in transformations.keys():
                 if transf in self.unit[:len(transf)]: ## Always first cm
-                    self.user_transform = transformations[self.unit[:len(transf)]]
-                    self.si_unit = self.unit.replace(self.unit[:len(transf)],"")    
+                    user_transform = transformations[self.unit[:len(transf)]]
+                    si_value = self.value * user_transform
+                    si_unit = self.unit[len(transf):]
+        return si_value,si_unit
+    def __str__(self):
+        print_value = "%.1e" % self.si_value
+        return super().__str__() if not self.transformed else f"{print_value} {self.si_unit}"
+
 
 
 class TimeMixin:
     def __init__(self, time : datetime = datetime.now()):
-        self.time = time
+        self.time = dt_parse(time)
     def __str__(self):
         return f"At time {self.time}"
 
 class DistanceMixin:
-    def __init__(self,distance:Measure)
+    def __init__(self,distance:Measure):
         assert distance.unit == "m", "Distance must be in m"
         self.distance = distance
     def __srt__(self):
         return f"At distance {self.distance}"
-
-
-        
-
--- tests/measure_test.py
+++ tests/measure_test.py
@@ -1,1 +1,10 @@
+from data_structures.measure import *
 
+def test_base_measure():
+    b_measure = BaseMeasure(10,"W")
+    assert str(b_measure) == "1.0e+01 W"
+def test_measure():
+    measure = Measure(5500,"Mmph",True)
+    assert str(measure) == "5.5e+09 mph"
+    measure_no_trans = Measure(100,"Mton",False)
+    assert str(measure_no_trans) == "1.0e+02 Mton"
